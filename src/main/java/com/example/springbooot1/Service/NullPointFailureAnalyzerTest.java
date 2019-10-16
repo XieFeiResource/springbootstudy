2246514e502b47d94c09b62a4bf2c51d70d6c5ef
Administrator <xfcomwork@163.com>
2019-10-16 11:30:04 +0800

    xiefeiresource

--- src/main/java/com/example/springbooot1/Service/NullPointFailureAnalyzerTest.java
+++ src/main/java/com/example/springbooot1/Service/NullPointFailureAnalyzerTest.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class NullPointFailureAnalyzerTest {
 
     public NullPointFailureAnalyzerTest(){
-        System.out.println("让你有很多冲突");
+        System.out.println("B提交，A来修改制造冲突");
         Integer a = null;
 //        System.out.println(a.equals("88"));
     }
