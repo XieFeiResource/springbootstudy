2d6218e9a9b336cd517f2bcb8376c4b80a0d7314
Administrator <xfcomwork@163.com>
2019-10-16 11:24:46 +0800

    xiefeiresourcetwo commit

--- src/main/java/com/example/springbooot1/Service/NullPointFailureAnalyzerTest.java
+++ src/main/java/com/example/springbooot1/Service/NullPointFailureAnalyzerTest.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 public class NullPointFailureAnalyzerTest {
 
     public NullPointFailureAnalyzerTest(){
+        System.out.println("让你有很多冲突");
         Integer a = null;
 //        System.out.println(a.equals("88"));
     }
