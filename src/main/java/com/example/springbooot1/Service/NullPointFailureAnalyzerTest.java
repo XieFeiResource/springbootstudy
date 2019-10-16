06cdd6a59cd1f33db0f09614cd3bf740a37a4ee1
Administrator <xfcomwork@163.com>
2019-10-16 16:51:54 +0800

    xiefeiresource

--- src/main/java/com/example/springbooot1/Service/NullPointFailureAnalyzerTest.java
+++ src/main/java/com/example/springbooot1/Service/NullPointFailureAnalyzerTest.java
@@ -6,20 +6,7 @@ import org.springframework.stereotype.Service;
 public class NullPointFailureAnalyzerTest {
 
     public NullPointFailureAnalyzerTest(){
-        System.out.println("B提交，A来修改制造冲突");
         Integer a = null;
 //        System.out.println(a.equals("88"));
-    }
-
-    public void methodA(){
-        System.out.println("这是方法");
-    }
-
-    public void methodB(){
-        System.out.println("decelop来啦");
-    }
-
-    public void methodC(){
-        System.out.println("提交到develop分支的代码");
     }
 }
