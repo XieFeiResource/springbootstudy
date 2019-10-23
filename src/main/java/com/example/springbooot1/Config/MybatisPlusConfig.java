51bf437f2380bd1d0ce18ff6bc91844d7951f1ad
Administrator <xfcomwork@163.com>
2019-10-23 18:03:34 +0800

    xiefeiresource

---
+++ src/main/java/com/example/springbooot1/Config/MybatisPlusConfig.java
@@ -0,0 +1,35 @@
+package com.example.springbooot1.Config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
+
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.qfclo.login.mapper")
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor  PaginationInterceptorpaginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+    /**
+     * 打印 sql
+     */
+    @Bean
+    public PerformanceInterceptor PerformanceInterceptorperformanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor =new PerformanceInterceptor();
+        //格式化sql语句
+        Properties properties =new Properties();
+        properties.setProperty("format", "faalse");
+        performanceInterceptor.setProperties(properties);
+        return performanceInterceptor;
+    }
+}
